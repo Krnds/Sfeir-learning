041ba9b7b500ef1539bf15279a28aa61e2618103
Karine DIAS <dias.k@sfeir.com>
2023-08-11 17:18:16 +0200

    :tada: Add chat-app project backbone

---
+++ reactive-programming/webflux-http2-webchat/src/main/java/com/karinedias/webfluxhttp2webchat/WebfluxHttp2WebchatApplication.java
@@ -0,0 +1,13 @@
+package com.karinedias.webfluxhttp2webchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxHttp2WebchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxHttp2WebchatApplication.class, args);
+	}
+
+}
---
+++ reactive-programming/webflux-http2-webchat/src/test/java/com/karinedias/webfluxhttp2webchat/WebfluxHttp2WebchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karinedias.webfluxhttp2webchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxHttp2WebchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
